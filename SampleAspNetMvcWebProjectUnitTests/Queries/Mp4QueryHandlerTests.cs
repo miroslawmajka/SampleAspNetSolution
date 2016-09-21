347994694c1a88ffbecd3d3572b8ebd803df4fa1
Mirek <go2mirek@gmail.com>
2016-09-21 13:18:25 +0100

    Refactored the unit test.

--- SampleAspNetMvcWebProjectUnitTests/Queries/Mp4QueryHandlerTests.cs
+++ SampleAspNetMvcWebProjectUnitTests/Queries/Mp4QueryHandlerTests.cs
@@ -13,8 +13,19 @@ namespace SampleVideoStreamingSiteUnitTests.Queries
     [TestClass]
     public class Mp4QueryHandlerTests
     {
-        const string ASSET_FOLDER_NAME = "Assets";
-        const string TEST_FILE_NAME = "TestFile.txt";
+        private const string ASSET_FOLDER_NAME = "Assets";
+        private const string TEST_FILE_NAME = "TestFile.txt";
+        private string testFileLocation;
+
+        [TestInitialize]
+        public void Init()
+        {
+            testFileLocation = Path.Combine(new string[] {
+                    Directory.GetCurrentDirectory(),
+                    ASSET_FOLDER_NAME,
+                    TEST_FILE_NAME
+                });
+        }
 
         [TestMethod]
         public void HandlerReturnsFullResponse()
@@ -22,12 +33,6 @@ namespace SampleVideoStreamingSiteUnitTests.Queries
             using (var mock = AutoMock.GetLoose())
             {
                 // Arrange
-                var testFileLocation = Path.Combine(new string[] {
-                    Directory.GetCurrentDirectory(),
-                    ASSET_FOLDER_NAME,
-                    TEST_FILE_NAME
-                });
-
                 mock.Mock<IQueryHandler<Mp4FileLocationQuery, string>>()
                     .Setup(x => x.Handle(It.IsAny<Mp4FileLocationQuery>()))
                     .Returns(testFileLocation);
@@ -39,10 +44,12 @@ namespace SampleVideoStreamingSiteUnitTests.Queries
                     FileName = TEST_FILE_NAME,
                     Request = new HttpRequestMessage(HttpMethod.Get, "http://www.test.com/")
                 });
-                
+
                 // Assert
+                Assert.IsTrue(result.IsSuccessStatusCode);
                 Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
                 Assert.AreEqual(typeof(StreamContent), result.Content.GetType());
+                Assert.AreEqual("OK", result.ReasonPhrase);
                 Assert.AreEqual("video/mp4", result.Content.Headers.ContentType.MediaType);
                 Assert.AreEqual("inline", result.Content.Headers.ContentDisposition.DispositionType);
                 Assert.AreEqual(TEST_FILE_NAME, result.Content.Headers.ContentDisposition.FileName);
@@ -54,18 +61,14 @@ namespace SampleVideoStreamingSiteUnitTests.Queries
         {
             using (var mock = AutoMock.GetLoose())
             {
-                var testFileLocation = Path.Combine(new string[] {
-                    Directory.GetCurrentDirectory(),
-                    ASSET_FOLDER_NAME,
-                    TEST_FILE_NAME
-                });
-
+                // Arrange
                 mock.Mock<IQueryHandler<Mp4FileLocationQuery, string>>()
                     .Setup(x => x.Handle(It.IsAny<Mp4FileLocationQuery>()))
                     .Returns(testFileLocation);
 
                 var subject = mock.Create<Mp4QueryHandler>();
 
+                // Act
                 var requestMessage = new HttpRequestMessage(HttpMethod.Get, "http://www.test.com/");
                 requestMessage.Headers.Range = new RangeHeaderValue(0, 0) {
                     Unit = "bytes"
@@ -76,7 +79,10 @@ namespace SampleVideoStreamingSiteUnitTests.Queries
                     Request = requestMessage
                 });
 
+                // Assert
+                Assert.IsTrue(result.IsSuccessStatusCode);
                 Assert.AreEqual(HttpStatusCode.PartialContent, result.StatusCode);
+                Assert.AreEqual("Partial Content", result.ReasonPhrase);
                 Assert.AreEqual(typeof(ByteRangeStreamContent), result.Content.GetType());
                 Assert.AreEqual("video/mp4", result.Content.Headers.ContentType.MediaType);
                 Assert.AreEqual("inline", result.Content.Headers.ContentDisposition.DispositionType);
@@ -89,26 +95,24 @@ namespace SampleVideoStreamingSiteUnitTests.Queries
         {
             using (var mock = AutoMock.GetLoose())
             {
-                var testFileLocation = Path.Combine(new string[] {
-                    Directory.GetCurrentDirectory(),
-                    ASSET_FOLDER_NAME,
-                    TEST_FILE_NAME
-                });
-
+                // Arrange
                 mock.Mock<IQueryHandler<Mp4FileLocationQuery, string>>()
                     .Setup(x => x.Handle(It.IsAny<Mp4FileLocationQuery>()))
                     .Returns(testFileLocation);
 
                 var subject = mock.Create<Mp4QueryHandler>();
 
+                // Act
                 var result = subject.Handle(new Mp4Query {
                     FileName = TEST_FILE_NAME,
                     Request = new HttpRequestMessage(HttpMethod.Head, "http://www.test.com/")
                 });
 
+                // Assert
+                Assert.IsTrue(result.IsSuccessStatusCode);
                 Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+                Assert.AreEqual("OK", result.ReasonPhrase);
                 Assert.AreEqual(typeof(StringContent), result.Content.GetType());
-                Assert.AreEqual("video/mp4", result.Content.Headers.ContentType.MediaType);
                 Assert.AreEqual("video/mp4", result.Content.Headers.ContentType.MediaType);
             }
         }
