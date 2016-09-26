67c5012988a870bac85c8348fd1a3b063da3e145
Mirek <go2mirek@gmail.com>
2016-09-26 23:44:34 +0100

    Added async and sync JS calls to a WebMethod.

---
+++ AspNetWebFormsWebProject/JsAsyncTest.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Web.Services;
+
+namespace AspNetWebFormsWebProject
+{
+    public partial class JsAsyncTest : System.Web.UI.Page
+    {
+        [WebMethod]
+        public static string GetCurrentTime(string name)
+        {
+            var timeString = DateTime.Now.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture);
+
+            Thread.Sleep(new Random().Next(1000, 5000));
+
+            return $"Hello, {name}. Current server time: {timeString}.";
+        }
+    }
+}
