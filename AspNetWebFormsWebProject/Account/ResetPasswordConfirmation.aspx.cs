16eb9aead79d396af62858f8547f837000621327
Miroslaw Majka <miroslaw.majka@c2software.com>
2016-09-27 09:12:09 +0100

    Renaming and re-organising.

--- AspNetWebFormsWebProject/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace AspNetWebFormsWebProject.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
---
+++ SampleAspNetWebFormsWebProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleAspNetWebFormsWebProject.Global" Language="C#" %>
