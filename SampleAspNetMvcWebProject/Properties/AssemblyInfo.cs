3bd57d9d24083b5ad0a460b4a9449061351b7cd1
Miroslaw Majka <miroslaw.majka@c2software.com>
2016-09-27 09:17:09 +0100

    Renaming.

--- SampleAspNetMvcWebProject/Properties/AssemblyInfo.cs
+++ SampleAspNetMvcWebProject/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SampleVideoStreamingSite")]
+[assembly: AssemblyProduct("SampleAspNetMvcWebProject")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
