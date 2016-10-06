a6fc854fa26f8bfa29613be3c2aeb7893c7f15a4
Miroslaw Majka <miroslaw.majka@c2software.com>
2016-10-06 15:51:06 +0100

    Added jQuery UI Nuget package and one sample from jQuery for the accordion widget.

---
+++ SampleAspNetMvcWebProject/Controllers/jQueryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SampleAspNetMvcWebProject.Controllers
+{
+    public class jQueryController : Controller
+    {
+        // GET: jQuery
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
